a701c72bd7abe248cf97aac7aa9e5ec1f5363342
AjithaVijayan <127471744+AjithaVijayan@users.noreply.github.com>
2023-04-29 19:29:40 +0530

    Add files via upload

---
+++ predict.py
@@ -0,0 +1,10 @@
+from input import mechine_learning as a
+import streamlit as st
+st.header("Labeling Demo")
+input = st.text_area("Enter the text", value="")
+l=[]
+l.append(input)
+if st.button("Predict"):
+    vec = a.vector.transform(l).toarray()
+    x=(str(list(a.naivebayes.predict(vec))[0]).replace('0', 'TECH').replace('1', 'BUSINESS').replace('2', 'SPORTS').replace('3','ENTERTAINMENT').replace('4','POLITICS'))
+    st.write("label:",x)
